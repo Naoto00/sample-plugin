1ecb7aeb3f1f4f144a162b0b1e54d37d210c2474
Naoto00 <naoto-nakazato@cybozu.co.jp>
2019-01-23 13:28:57 +0900

    fix js files for updated eslint rules

--- src/js/config.js
+++ src/js/config.js
@@ -1,106 +1,111 @@
 jQuery.noConflict();
 
 (function($, PLUGIN_ID) {
-    'use strict';
-    var $accessKey = $('#accessKey');
-    var $bfrDropDown = $('#select_before_exchange');
-    var $aftDropDown = $('#select_after_exchange');
-    var $dateDropDown = $('#date');
-    var config = kintone.plugin.app.getConfig(PLUGIN_ID);
+  'use strict';
+  var $accessKey = $('#accessKey');
+  var $bfrDropDown = $('#select_before_exchange');
+  var $aftDropDown = $('#select_after_exchange');
+  var $dateDropDown = $('#date');
+  var config = kintone.plugin.app.getConfig(PLUGIN_ID);
 
-    var requestURL = 'http://www.apilayer.net/api/historical';
+  var requestURL = 'http://www.apilayer.net/api/historical';
 
-    function getSettingsUrl() {
-        return '/k/admin/app/flow?app=' + kintone.app.getId();
-    }
+  function getSettingsUrl() {
+    return '/k/admin/app/flow?app=' + kintone.app.getId();
+  }
 
-    // Get information of each field
-    function setDropDownForSpace() {
-        kintone.api(kintone.api.url('/k/v1/preview/app/form/fields', true), 'GET',
-            {'app': kintone.app.getId()}).then(function(resp) {
-            for (var key in resp.properties) {
-                if (!resp.properties.hasOwnProperty(key)) {continue;}
-                switch (resp.properties[key].type) {
-                    case 'NUMBER':
-                        var $opText = $('<option></option>');
-                        $opText.attr('value', resp.properties[key].code);
-                        $opText.text(resp.properties[key].label);
-                        $bfrDropDown.append($opText);
-                        $aftDropDown.append($opText.clone());
-                        break;
-                    case 'DATE':
-                        var $opDate = $('<option></option>');
-                        $opDate.attr('value', resp.properties[key].code);
-                        $opDate.text(resp.properties[key].label);
-                        $dateDropDown.append($opDate);
-                }
-            }
-            // Set default values
-            if (config.beforeExchange) {
-                $bfrDropDown.val(config.beforeExchange);
-            }
-            if (config.afterExchange) {
-                $aftDropDown.val(config.afterExchange);
-            }
-            if (config.date) {
-                $dateDropDown.val(config.date);
-            }
-        });
-    }
-
-    $(document).ready(function() {
-        // Set default values
-        if (config.accessKey) {
-            $accessKey.val(config.accessKey);
+  // Get information of each field
+  function setDropDownForSpace() {
+    kintone.api(kintone.api.url('/k/v1/preview/app/form/fields', true), 'GET',
+      {'app': kintone.app.getId()}).then(function(resp) {
+      var $opText = $('<option></option>');
+      var $opDate = $('<option></option>');
+      Object.keys(resp.properties).forEach(function(key) {
+        if (!resp.properties.hasOwnProperty(key)) {
+          return;
         }
-        if (config.currencies) {
-            $('#' + config.currencies).prop('checked', true);
+        switch (resp.properties[key].type) {
+          case 'NUMBER':
+            $opText.attr('value', resp.properties[key].code);
+            $opText.text(resp.properties[key].label);
+            $bfrDropDown.append($opText);
+            $aftDropDown.append($opText.clone());
+            break;
+          case 'DATE':
+            $opDate.attr('value', resp.properties[key].code);
+            $opDate.text(resp.properties[key].label);
+            $dateDropDown.append($opDate);
         }
-        // Set drop-down list
-        setDropDownForSpace();
-        // Set input values when 'Save' button is clicked
-        $('#check-plugin-submit').click(function() {
-            var $targetCurrency = $('[name=radio]:checked');
-
-            // Check required fields
-            var requiredParams = [
-                $accessKey.val(),
-                $bfrDropDown.val(),
-                $aftDropDown.val(),
-                $dateDropDown.val()
-            ];
-            for (var i in requiredParams) {
-                if (!requiredParams[i] || requiredParams[i] === '-----') {
-                    alert('Please set required field(s)');
-                    return;
-                }
-            }
-            if ($bfrDropDown.val() === $aftDropDown.val()) {
-                alert('USD and Output Currency must be set as different fields');
-                return;
-            }
-
-            var body = {
-                'access_key': $accessKey.val(),
-                'currencies': $targetCurrency.val()
-            };
-            kintone.plugin.app.setProxyConfig(requestURL, 'GET', {}, body, function() {
-                kintone.plugin.app.setConfig({
-                    'accessKey': $accessKey.val(),
-                    'beforeExchange': $bfrDropDown.val(),
-                    'currencies': $targetCurrency.val(),
-                    'afterExchange': $aftDropDown.val(),
-                    'date': $dateDropDown.val()
-                }, function() {
-                    alert('Please update the app!');
-                    window.location.href = getSettingsUrl();
-                });
-            });
-        });
-        // Process when 'Cancel' is clicked
-        $('#check-plugin-cancel').click(function() {
-            history.back();
-        });
+      });
+      // Set default values
+      if (config.beforeExchange) {
+        $bfrDropDown.val(config.beforeExchange);
+      }
+      if (config.afterExchange) {
+        $aftDropDown.val(config.afterExchange);
+      }
+      if (config.date) {
+        $dateDropDown.val(config.date);
+      }
     });
+  }
+
+  $(document).ready(function() {
+    // Set default values
+    if (config.accessKey) {
+      $accessKey.val(config.accessKey);
+    }
+    if (config.currencies) {
+      $('#' + config.currencies).prop('checked', true);
+    }
+    // Set drop-down list
+    setDropDownForSpace();
+    // Set input values when 'Save' button is clicked
+    $('#check-plugin-submit').click(function() {
+      var $targetCurrency = $('[name=radio]:checked');
+
+      // Check required fields value
+      var requiredParams = [
+        $accessKey.val(),
+        $bfrDropDown.val(),
+        $aftDropDown.val(),
+        $dateDropDown.val()
+      ];
+      var body = {
+        'access_key': $accessKey.val(),
+        'currencies': $targetCurrency.val()
+      };
+      var paramIsEmpty;
+      requiredParams.forEach(function(i) {
+        if (!requiredParams[i] || requiredParams[i] === '-----') {
+          paramIsEmpty = true;
+        }
+      });
+      if (paramIsEmpty) {
+        alert('Please set required field(s)');
+        return;
+      }
+      if ($bfrDropDown.val() === $aftDropDown.val()) {
+        alert('USD and Output Currency must be set as different fields');
+        return;
+      }
+      kintone.plugin.app.setProxyConfig(requestURL, 'GET', {}, body, function() {
+        kintone.plugin.app.setConfig({
+          'accessKey': $accessKey.val(),
+          'beforeExchange': $bfrDropDown.val(),
+          'currencies': $targetCurrency.val(),
+          'afterExchange': $aftDropDown.val(),
+          'date': $dateDropDown.val()
+        }, function() {
+          alert('Please update the app!');
+          window.location.href = getSettingsUrl();
+        });
+      });
+    });
+    // Process when 'Cancel' is clicked
+    $('#check-plugin-cancel').click(function() {
+      history.back();
+    });
+  });
 
 })(jQuery, kintone.$PLUGIN_ID);
--- src/js/desktop.js
+++ src/js/desktop.js
@@ -1,63 +1,64 @@
 jQuery.noConflict();
 
 (function($, PLUGIN_ID) {
-    'use strict';
+  'use strict';
 
-    // Get plug-in configuration settings
-    var CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
-    if (!CONFIG) {
-        return false;
+  // Get plug-in configuration settings
+  var CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
+  var events = [
+    'app.record.create.change.' + CONFIG.beforeExchange,
+    'app.record.edit.change.' + CONFIG.beforeExchange,
+    'app.record.create.change.' + CONFIG.date,
+    'app.record.edit.change.' + CONFIG.date
+  ];
+
+  if (!CONFIG) {
+    return;
+  }
+
+  kintone.events.on(events, function(event) {
+    var record = event.record;
+    var dtVal = record[CONFIG.date].value;
+    var bfrExchangeVal = record[CONFIG.beforeExchange].value;
+    var dtisFuture = moment(dtVal).isAfter(moment());
+    var requestURL = 'http://www.apilayer.net/api/historical';
+    // validate date field and exchanged filed
+    if (record[CONFIG.date].error || record[CONFIG.beforeExchange].error || event.error) {
+      return event;
     }
+    if ((!dtVal || dtisFuture) && !bfrExchangeVal) {
+      record[CONFIG.date].error = 'The date value is invalid';
+      record[CONFIG.beforeExchange].error = 'The exchanged value is empty';
+      return event;
+    } else if (!dtVal || dtisFuture) {
+      record[CONFIG.date].error = 'The date value is invalid';
+      record[CONFIG.beforeExchange].error = null;
+      return event;
+    } else if (!bfrExchangeVal) {
+      record[CONFIG.beforeExchange].error = 'The exchanged value is empty';
+      record[CONFIG.date].error = null;
+      return event;
+    }
+    record[CONFIG.beforeExchange].error = null;
+    record[CONFIG.date].error = null;
+    requestURL += '?date=' + dtVal;
+    kintone.plugin.app.proxy(PLUGIN_ID, requestURL, 'GET', {}, {}).then(function(resp) {
+      var rate = bfrExchangeVal * JSON.parse(resp[0]).quotes['USD' + CONFIG.currencies];
+      var setRecord = {
+        'record': record
+      };
 
-    var events = [
-        'app.record.create.change.' + CONFIG.beforeExchange,
-        'app.record.edit.change.' + CONFIG.beforeExchange,
-        'app.record.create.change.' + CONFIG.date,
-        'app.record.edit.change.' + CONFIG.date
-    ];
-
-    kintone.events.on(events, function(event) {
-        var record = event.record;
-        var dtVal = record[CONFIG.date].value;
-        var bfrExchangeVal = record[CONFIG.beforeExchange].value;
-        var dtisFuture = moment(dtVal).isAfter(moment());
-        // validate date field and exchanged filed
-        if (record[CONFIG.date].error || record[CONFIG.beforeExchange].error || event.error) {
-            return event;
-        }
-        if ((!dtVal || dtisFuture) && !bfrExchangeVal) {
-            record[CONFIG.date].error = 'The date value is invalid';
-            record[CONFIG.beforeExchange].error = 'The exchanged value is empty';
-            return event;
-        } else if (!dtVal || dtisFuture) {
-            record[CONFIG.date].error = 'The date value is invalid';
-            record[CONFIG.beforeExchange].error = null;
-            return event;
-        } else if (!bfrExchangeVal) {
-            record[CONFIG.beforeExchange].error = 'The exchanged value is empty';
-            record[CONFIG.date].error = null;
-            return event;
-        }
-        record[CONFIG.beforeExchange].error = null;
-        record[CONFIG.date].error = null;
-
-        var requestURL = 'http://www.apilayer.net/api/historical';
-        requestURL += '?date=' + dtVal;
-        kintone.plugin.app.proxy(PLUGIN_ID, requestURL, 'GET', {}, {}).then(function(resp) {
-            if (!JSON.parse(resp[0]).success) {
-                return kintone.Promise.reject(new Error(JSON.parse(resp[0]).error.info));
-            }
-            var rate = bfrExchangeVal * JSON.parse(resp[0]).quotes['USD' + CONFIG.currencies];
-            record[CONFIG.afterExchange].value = rate;
-            var setRecord = {
-                'record': record
-            };
-            kintone.app.record.set(setRecord);
-        }).catch(function(err) {
-            alert(err);
-        });
-        return event;
-
+      if (!JSON.parse(resp[0]).success) {
+        kintone.Promise.reject(new Error(JSON.parse(resp[0]).error.info));
+        return;
+      }
+      record[CONFIG.afterExchange].value = rate;
+      kintone.app.record.set(setRecord);
+    }).catch(function(err) {
+      alert(err);
     });
+    return event;
+
+  });
 
 })(jQuery, kintone.$PLUGIN_ID);
